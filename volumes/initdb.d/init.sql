a604760345bb3ff7ee7398b97b97792e0be8fcb0
jiro4989 <jiroron666@gmail.com>
2020-12-06 05:39:57 +0900

    fix

--- volumes/initdb.d/init.sql
+++ volumes/initdb.d/init.sql
@@ -2,7 +2,7 @@
 -- CREATE DATABASE kintai;
 -- GRANT ALL PRIVILEGES ON DATABASE kintai TO kintai;
 
-CREATE TABLE IF NOT EXISTS "user"
+CREATE TABLE IF NOT EXISTS member
 (id SERIAL, email TEXT, password TEXT,
  PRIMARY KEY (id),
  UNIQUE (email));
