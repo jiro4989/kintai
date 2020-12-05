49ae58f69e67e42013a5ee3e828df1c13b155bb0
jiro4989 <jiroron666@gmail.com>
2020-12-06 00:12:52 +0900

    とりあえず

--- server/src/server/handler.clj
+++ server/src/server/handler.clj
@@ -1,11 +1,18 @@
 (ns server.handler
-  (:require [compojure.core :refer [defroutes context GET]]
+  (:require [compojure.core :refer [defroutes context GET POST]]
             [compojure.route :as route]))
 
 (defn api-signup [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body "{\"signup ok\"}"})
+  (do
+    (println req)
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body "{\"signin ok\"}"}))
+
+; (defn api-signup [req]
+;   {:status 200
+;    :headers {"Content-Type" "application/json"}
+;    :body "{\"signup ok\"}"})
 
 (defn api-signin [req]
   {:status 200
@@ -13,6 +20,8 @@
    :body "{\"signin ok\"}"})
 
 (defroutes handler
-  (GET "/api/v1/signup" req api-signup)
-  (GET "/api/v1/signin" req api-signin)
+  (context "/api/v1" _
+           (POST "/signup" req api-signup)
+           (POST "/signin" req api-signin)
+           )
   (route/not-found "{\"message\":\"api not found\"}"))
