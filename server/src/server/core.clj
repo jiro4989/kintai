d9f9e1f475d43ba364096509dce4cd4da9578364
jiro4989 <jiroron666@gmail.com>
2020-12-05 23:28:53 +0900

    一旦ハローワールドできるようにした

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,7 +1,13 @@
 (ns server.core
+  (:use ring.adapter.jetty)
   (:gen-class))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  ""
   [& args]
-  (println "Hello, World!"))
+  (run-jetty handler {:port 5000}))
