8c4ec4fb80860e7252cd438ccb300a0a759c4a1e
jiro4989 <jiroron666@gmail.com>
2020-12-06 06:12:12 +0900

    session

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -3,11 +3,13 @@
   (:require [compojure.core :refer [defroutes context GET]]
             [compojure.route :as route]
             [server.handler :refer [handler]]
-            [ring.middleware.params :refer [wrap-params]])
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.session :refer [wrap-session]])
   (:gen-class))
 
 (defn -main
   ""
   [& args]
   (run-jetty (-> handler
-                 wrap-params) {:port 5000}))
+                 wrap-params
+                 wrap-session) {:port 5000}))
