395fb4647a2f8d2e2230c29e9cc160e47d2593c5
jiro4989 <jiroron666@gmail.com>
2020-12-06 15:40:03 +0900

    front: signup APIを呼ぶように変更

--- front/src/Signup.js
+++ front/src/Signup.js
@@ -1,9 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function Signup() {
+  const [result, setResult] = useState("");
+
+  const obj = {email: "unko@gmail.com", password: "unko"}
+  const body = Object.keys(obj).map((key)=>key+"="+encodeURIComponent(obj[key])).join("&");
+
+  const onClick = () => {
+    fetch("http://localhost:5000/api/v1/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "mode": "cors",
+        "cache": "no-cache",
+      },
+      body: body,
+    }).then((resp) => resp.json())
+    .then((resp) => { setResult(resp) })
+    .catch(console.error);
+  }
+
   return (
     <div className="Signup">
       Signup
+      <button type="button" onClick={onClick}>
+        Signup
+      </button>
+      <div>
+        Result: {result}
+      </div>
     </div>
   );
 }
