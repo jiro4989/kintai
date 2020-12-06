c3b90cdcf39e173e6cbfbdce67f94d437c6da8f7
jiro4989 <jiroron666@gmail.com>
2020-12-06 17:56:59 +0900

    front: メンバー一覧を表示できるように

--- front/src/Members.js
+++ front/src/Members.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Members() {
   const [members, setMembers] = useState([]);
 
-  fetch("http://localhost/api/v1/members", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "mode": "cors",
-      "cache": "no-cache",
-      "credentials": 'include'
-    },
-  })
-  .then((resp) => resp.json())
-  .then((resp) => { setMembers(resp.members) })
-  .catch((resp) => { console.error(resp) });
+  useEffect(() => {
+    fetch("http://localhost/api/v1/members", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "mode": "cors",
+        "cache": "no-cache",
+        "credentials": 'include'
+      },
+    })
+    .then((resp) => resp.json())
+    .then((resp) => { setMembers(resp.members) })
+    .catch((resp) => { console.error(resp) });
+  }, [])
 
-  let memberHTML = [];
-  for (let m in members) {
-    memberHTML.push(
-      <tr>
-        <td>
-          m.id
-        </td>
-        <td>
-          m.email
-        </td>
-      </tr>
-    )
-  }
+  const memberHTML = members.map((m) => {
+    return <tr key={m.id}>
+      <td>
+        {m.id}
+      </td>
+      <td>
+        {m.email}
+      </td>
+    </tr>
+  })
 
   return (
     <div className="Members">
