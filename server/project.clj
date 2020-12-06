18e1199f40e885d891bfdc2cea39e9dace386155
jiro4989 <jiroron666@gmail.com>
2020-12-06 15:39:44 +0900

    backend: CORSのミドルウェアを追加

--- server/project.clj
+++ server/project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.2"]
+                 [ring-cors "0.1.13"]
                  [compojure "1.6.2"]
                  [org.clojure/java.jdbc "0.7.11"]
                  [clj-postgresql "0.7.0"]
